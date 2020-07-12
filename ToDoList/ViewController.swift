0a301e065a0ccf14784bdc4a459e10f5ba9b2a81
Murilo Araujo <muernandes@icloud.com>
2020-07-11 21:04:37 -0300

    Implemented todo functionality

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -6,14 +6,81 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextViewDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var itemName = [NSManagedObject]()
+    var titleTextField: UITextField!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemName.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let title = itemName[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemcell")
+        cell?.textLabel?.text = title.value(forKey: "Title") as? String
+        return cell!
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func titleTextField(_ textField: UITextField) {
+        titleTextField = textField
+        titleTextField.placeholder = "Item Name"
+    }
+    
+    
+    @IBAction func addButtonTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Add your item", message: "Add your item name below", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Save", style: .default, handler: self.save))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addTextField(configurationHandler: titleTextField)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func save(alert: UIAlertAction) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Title", in: context)!
+        let theTitle = NSManagedObject(entity: entity, insertInto: context)
+        theTitle.setValue(titleTextField.text, forKey: "Title")
+        try? context.save()
+        itemName.append(theTitle)
+        self.tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
